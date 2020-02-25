addb59732e885d4cbbb87001a752d71343c47994
marcol13 <56632321+marcol13@users.noreply.github.com>
2020-02-25 15:10:21 +0100

    Reduce code to paragraph call slip

--- script.js
+++ script.js
@@ -14,4 +14,20 @@ btn = document.querySelector("button.submit-button")
 btn.addEventListener("click", () => {code = getText()})
 
 function reduceCode(str){
+    findText = "<p class=\"hws phraseEntity\">"
+    result = ""
+    while(str.indexOf(findText) != -1){
+        indexB = str.indexOf(findText)
+        indexE = str.indexOf("</p>", indexB)
+        temp = str.slice(indexB, indexE)
+        result += temp
+        //console.log(temp)
+        str = str.slice(indexE)
+    }
+    return result
+}
+code = reduceCode(code)
+
+function toDictionary(str){
+    
 }
