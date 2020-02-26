740e289ac9555d751fa0556512783d07980c5e62
marcol13 <56632321+marcol13@users.noreply.github.com>
2020-02-26 15:52:46 +0100

    Create function that delete unwanted chars

--- script.js
+++ script.js
@@ -33,12 +33,31 @@ function reduceCode(str){
 }
 //code = reduceCode(code)
 
+function deleteUnwanted(str){
+    signs = [`=`, `= ↵`]
+    special = [[`&quot;`,`"`],[`&apos;`,`'`],[]]
+    for(let i of signs){
+        str = str.replace(new RegExp(i ,"g"),"")
+    }
+    for(let i of special){
+        //str = str.replace(new RegExp(i[0],"g"),i[1]) dont know why it doesnt work
+        while(str.indexOf(i[0]) != -1){
+            str = str.replace(i[0],i[1])
+        }
+    }
+    while(str.indexOf("<") != -1){
+        let beginIndex = str.indexOf("<")
+        let endIndex = str.indexOf(">")
+        str = str.replace(str.substring(beginIndex, endIndex+1),"")
+    }
+    return str.trim()
+}
+
 function toDictionary(str, arr){
     const englishTextBFind = `<span class="hw">`
     const englishTextEFind = `<`
     const polishTextBFind = `</a></span></span>`
     const polishTextEFind = `&nbsp;`
-    replaceItems = [`</span>`,`=`]
     console.log(str.indexOf(englishTextBFind))
     while(str.indexOf(englishTextBFind) != -1 && str.indexOf(polishTextBFind) != -1){
         console.log(str.indexOf(englishTextBFind) + englishTextBFind.length)
@@ -48,9 +67,9 @@ function toDictionary(str, arr){
         let indexEnd = str.indexOf(polishTextEFind, polishTextB)
         polishText = str.slice(polishTextB, indexEnd)
 
-        polishText = polishText.replace(`</span>`,"")
-        polishText = polishText.replace(`=`,"")
-        arr.push(new CallSlip(englishText.trim(), polishText.trim()))
+        // polishText = polishText.replace(`</span>`,"")
+        // polishText = polishText.replace(`=`,"")
+        arr.push(new CallSlip(deleteUnwanted(englishText), deleteUnwanted(polishText)))
         str = str.slice(indexEnd)
     }
 }
